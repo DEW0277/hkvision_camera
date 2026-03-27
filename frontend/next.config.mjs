a185ddf6b258c15e0aa35c65c57bb274efec5452
JalolddinCoder <jaloliddindevloper@gmail.com>
2026-03-27 22:01:47 +0500

    added faceID cameradan xodimlarni ro'yxatini olib kelish

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: "/face-api/:path*",
-        destination: "http://213.230.118.15:4001/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
